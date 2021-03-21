bab43885ef3783cc01707de89a41829677cd0d3c
Lucas Nicoleti <56062541+Lnicoleti@users.noreply.github.com>
2021-03-21 02:13:10 -0300

    Validação de módulos por referência ao bloco zero
    PS C:\Users\lucas\LSN\Dev\Blockchain> node main.js
    {
        "chain": [
            {
                "index": 0,
                "timestamp": "01/01/2021",
                "data": "Genesis Block",
                "previousHash": "0",
                "hash": ""
            },
            {
                "index": 1,
                "timestamp": "21/03/2021",
                "data": {
                    "emission": 10
                },
                "previousHash": "",
                "hash": "71e4c9587ef50392d05ea3e1753022495c86f1990409d886fa1ff4417bd4a2d8"
            },
            {
                "index": 2,
                "timestamp": "22/03/2021",
                "data": {
                    "emission": 20
                },
                "previousHash": "71e4c9587ef50392d05ea3e1753022495c86f1990409d886fa1ff4417bd4a2d8",
                "hash": "cb3000739e3adbd5b8a46b8b6cba8b166ad5d7539441c8c8c6180b4241253e17"
            }
        ]
    }

---
+++ main.js
@@ -0,0 +1,56 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = '';
+    }
+
+    calculateHash(){
+return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block (0, "01/01/2021", "Genesis Block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash){
+                return false;
+            }
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+    }
+
+}
+
+let ECO2 = new Blockchain();
+ECO2.addBlock(new Block(1, "21/03/2021", {emission: 10}));
+ECO2.addBlock(new Block(2, "22/03/2021", {emission: 20}));
+
+console.log(JSON.stringify(ECO2, null, 4));
